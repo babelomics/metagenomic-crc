980619b4b82da358bb6b468488d53b3a1dc1b3ee
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2020-05-20 19:21:02 +0200

    Add methods for rank stability estimation.

--- mlgut/models.py
+++ mlgut/models.py
@@ -10,6 +10,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import FunctionTransformer, KBinsDiscretizer
 from sklearn.feature_selection import SelectFpr
 from interpret.glassbox import ExplainableBoostingClassifier
+from scipy.spatial.distance import pdist
+from mlgut.rbo import rbo_dict
 
 
 def get_model(profile: str, selector=True) -> Pipeline:
@@ -82,3 +84,32 @@ def get_taxonomic_model(selector=True) -> Pipeline:
         )
 
     return model
+
+
+def compute_rbo_mat(rank_mat_filt, p=0.999, filt=True):
+    if filt:
+        # safe filter to speed up the computation
+        rank_mat_filt = rank_mat_filt.loc[rank_mat_filt.any(axis=1), :]
+
+    col_dict_list = np.array(
+        [rank_mat_filt[col].to_dict() for col in rank_mat_filt.columns]
+    ).reshape(-1, 1)
+    distmat = pdist(col_dict_list, lambda x, y: 1 - rbo_dict(x[0], y[0], p=p)[-1])
+
+    return distmat
+
+
+def compute_support_ebm(model, quantile=None):
+    #TODO: check if trained
+    support = model["selector"].get_support()
+    
+    coefs = np.zeros(support.size)
+    
+    ebm = model["estimator"]
+    ebm_global = ebm.explain_global()
+    data = ebm_global.data()
+    coefs[support] = np.array(data["scores"])
+    
+    support = support * 1
+    
+    return support, coefs
