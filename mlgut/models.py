32f9137aaa3cb3f78358943bba4353f8365e2278
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2020-05-20 13:17:50 +0200

    Add the possibility to build a model without FS.

--- mlgut/models.py
+++ mlgut/models.py
@@ -12,20 +12,26 @@ from sklearn.feature_selection import SelectFpr
 from interpret.glassbox import ExplainableBoostingClassifier
 
 
-def get_model(profile: str) -> Pipeline:
+def get_model(profile: str, selector=True) -> Pipeline:
     """[summary]
 
     Parameters
     ----------
     profile : str
         [description]
+    selector : bool, optional
+        [description], by default True
 
     Returns
     -------
     Pipeline
         [description]
-    """
 
+    Raises
+    ------
+    NotImplementedError
+        [description]
+    """
     if profile.lower() in ["taxo", "taxonomic", "centrifuge"]:
         model = get_taxonomic_model()
     else:
@@ -34,27 +40,45 @@ def get_model(profile: str) -> Pipeline:
     return model
 
 
-def get_taxonomic_model() -> Pipeline:
+def get_taxonomic_model(selector=True) -> Pipeline:
     """[summary]
+
+    Parameters
+    ----------
+    selector : bool, optional
+        [description], by default True
 
     Returns
     -------
     Pipeline
         [description]
     """
-
-    model = Pipeline(
-        [
-            ("transformer", FunctionTransformer(np.log1p)),
-            ("discretizer", KBinsDiscretizer(n_bins=4, encode="ordinal")),
-            ("selector", SelectFpr()),
-            (
-                "estimator",
-                ExplainableBoostingClassifier(
-                    n_estimators=32, n_jobs=-1, random_state=42
+    if selector:
+        model = Pipeline(
+            [
+                ("transformer", FunctionTransformer(np.log1p)),
+                ("discretizer", KBinsDiscretizer(n_bins=4, encode="ordinal")),
+                ("selector", SelectFpr()),
+                (
+                    "estimator",
+                    ExplainableBoostingClassifier(
+                        n_estimators=32, n_jobs=-1, random_state=42
+                    ),
                 ),
-            ),
-        ]
-    )
+            ]
+        )
+    else:
+        model = Pipeline(
+            [
+                ("transformer", FunctionTransformer(np.log1p)),
+                ("discretizer", KBinsDiscretizer(n_bins=4, encode="ordinal")),
+                (
+                    "estimator",
+                    ExplainableBoostingClassifier(
+                        n_estimators=32, n_jobs=-1, random_state=42
+                    ),
+                ),
+            ]
+        )
 
     return model
