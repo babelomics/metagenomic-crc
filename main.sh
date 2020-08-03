9e214aeafdb230b26f771a2c69f8f2fc540f0ebb
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2020-08-03 17:00:10 +0200

    Change job and log naming schema.

--- main.sh
+++ main.sh
@@ -4,13 +4,15 @@ mode=$1
 
 condition="CRC"
 declare -a profiles=( "OGs" "centrifuge" "KEGG_KOs" )
-job_name="mlgut${profile}"
-path="data/paper/${condition}_${profile}"
-mkdir -p ${path}
-err="${path}/log.err"
-out="${path}/log.out"
 
 for profile in "${profiles[@]}"; do
+
+    job_name="mlgut_$condition$_${profile}_${mode}"
+    path="data/paper/${condition}_${profile}"
+    mkdir -p ${path}
+    err="${path}/${job_name}.err"
+    out="${path}/${job_name}.out"
+
     if [[ "$mode" == "train" ]]; then
         sbatch -J ${job_name} -N 1 -c 24 -e $err -o $out --wrap="ml anaconda2; conda activate ./.venv; python main.py ${condition} ${profile} ${path}"
     else
