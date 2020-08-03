e5d20a76009523a34f74defb9cc6b537a50216ec
Carlos Loucera <carlos.loucera@juntadeandalucia.es>
2020-08-03 16:53:46 +0200

    Add multinode and signficance options.

--- main.sh
+++ main.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+mode=$1
+
 condition="CRC"
-profile="OGs"
+declare -a profiles=( "OGs" "centrifuge" "KEGG_KOs" )
 job_name="mlgut${profile}"
 path="data/paper/${condition}_${profile}"
 mkdir -p ${path}
 err="${path}/log.err"
 out="${path}/log.out"
 
-sbatch -J ${job_name} -N 1 -c 24 -e $err -o $out --wrap="ml anaconda2; conda activate ./.venv; python main.py ${condition} ${profile} ${path}"
+for profile in "${profiles[@]}"; do
+    if [[ "$mode" == "train" ]]; then
+        sbatch -J ${job_name} -N 1 -c 24 -e $err -o $out --wrap="ml anaconda2; conda activate ./.venv; python main.py ${condition} ${profile} ${path}"
+    else
+        sbatch -J ${job_name} -N 1 -c 24 -e $err -o $out --wrap="ml anaconda2; conda activate ./.venv; python significance.py ${condition} ${profile} ${path}"
+    fi
+done
